db99db3f8b1de93d355df4a3720074ac5989d2aa
allbinary-hub <rakesh.sarmah.in@gmail.com>
2025-04-23 04:26:45 +0000

    dataypes

---
+++ .devcontainer/01_basics/data_types.js
@@ -0,0 +1,24 @@
+ "use strict"; //treat as new standard 
+ //alert((3+3) //we are using noejs, applicable only in browser
+
+
+console.log("Rakesh"); //code should be redable and future improvement 
+
+console.log(7+7+8);
+
+
+let name="hello" //String data type double quote
+let age = 45  //int without double quote
+let isLoggedin = null // bool type data 
+ 
+//number => 2^53 bigit ,string="",bool 0 or 1 ; 
+//null => standalone value 
+//undefined =>
+//symbol => uniquiness
+
+
+//object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
+
