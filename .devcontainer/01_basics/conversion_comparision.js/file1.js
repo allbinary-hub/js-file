f93ce42b3ee940fcb6e102234f502c7b9ceae20c
allbinary-hub <rakesh.sarmah.in@gmail.com>
2025-04-28 18:24:28 +0000

    file1

---
+++ .devcontainer/01_basics/conversion_comparision.js/file1.js
@@ -0,0 +1,29 @@
+/*let score = 33;
+console.log(typeof score);
+//console.log(valueOf score)
+
+let valueInNumber = Number(score)
+//console.log(typeOf valueInNumber)
+console.log(valueInNumber)
+
+//*******************************Operations *******************/
+
+let value =3
+let negValue=-value;
+console.log(negValue);
+
+console.log(6*6)
+
+console.log(6/6)
+
+console.log(6%9)
+
+
+console.log(6+6)
+
+
+let star1="hii"
+let star2="rakesh"
+
+let str3=star1+star2
+console.log(str3);
